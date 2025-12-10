579e2ce01732f745b62602a40faebb965b417df6
Prem Kumar <premvcourse1845@gmail.com>
2025-12-10 20:08:53 +0530

    Revert "Testing Revert Commit"
    This reverts commit e992bc55eb887151e1dceda7a048686c86e647e9.

--- Demo.java
+++ Demo.java
@@ -4,7 +4,6 @@ public class Demo {
     public static void main(String[] args){
 
         System.out.println("In java training");
-		// Testing Git changes.
 
         // If condition Example
 //        // byte, short, int , long, float, double, char, boolean
